b8265df5a1bf7390af29ab5529e9dce00a890f9c
anthony blackant <blackant02@live.com>
2019-03-16 01:08:14 -0400

    3rd commit

--- Anthony Corbin Kruse and Kurt Devilbiss/Form1.Designer.cs
+++ Anthony Corbin Kruse and Kurt Devilbiss/Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(293, 218);
             this.pictureBox1.TabIndex = 0;
